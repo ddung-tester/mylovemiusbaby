6bd47c884bbe8cb40bfb4230939381c8ab80279d
ddung-tester <ddung.tester@gmail.com>
2026-02-05 00:46:16 +0700

    Fix deployment: use HashRouter and relative paths

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // Relative base path for flexible deployment
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
